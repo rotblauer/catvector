d9c2b4b41d334d6db4e0d73fe4d5d208be2c5956
ia <isaac.ardis@gmail.com>
2024-09-12 20:52:22 -0600

    weighted election for stop detoct
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- trip_detector.go
+++ trip_detector.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/montanaflynn/stats"
+	"github.com/paulmach/orb/planar"
 	"time"
 
 	"github.com/paulmach/orb"
 	"github.com/paulmach/orb/geo"
 	"github.com/paulmach/orb/geojson"
-	"github.com/paulmach/orb/planar"
 )
 
 const segmentBoundEpsilon = 1e-5
@@ -17,33 +18,32 @@ const segmentBoundEpsilon = 1e-5
 // Trips are defined as a series of points that are moving, structured as LineStrings.
 // Stops are defined as a series of points that are stationary, structured as Points.
 type TripDetector struct {
-	DwellTime           time.Duration
-	TripStartTime       time.Duration
-	SpeedThreshold      float64
-	StopClusterDistance float64
-	lastNPoints         TracksGeoJSON
-	intervalPoints      TracksGeoJSON
-	Tripping            bool
+	DwellTime      time.Duration
+	TripStartTime  time.Duration
+	SpeedThreshold float64
+	DwellDistance  float64
+	lastNPoints    TracksGeoJSON
+	intervalPoints TracksGeoJSON
+	Tripping       bool
 
 	// MotionStateReason is a string that describes the reason for the current state of the TripDetector.
 	MotionStateReason string
 
-	// StoppedLoc is the centroid of the last N points when the TripDetector is in a stopped state.
-	// It is currently not used.
-	StoppedLoc orb.Point
+	// intervalPtsCentroid is the centroid of the last dwell interval points.
+	intervalPtsCentroid orb.Point
 
 	segmentIntersectionGauge float64
 }
 
-func NewTripDetector(dwellTime, tripStartTime time.Duration, speedThreshold, stopClusterDistance float64) *TripDetector {
+func NewTripDetector(dwellTime, tripStartTime time.Duration, speedThreshold, dwellDistance float64) *TripDetector {
 	return &TripDetector{
-		DwellTime:           dwellTime,
-		TripStartTime:       tripStartTime,
-		SpeedThreshold:      speedThreshold,
-		StopClusterDistance: stopClusterDistance,
-		Tripping:            false,
-		lastNPoints:         TracksGeoJSON{},
-		intervalPoints:      TracksGeoJSON{},
+		DwellTime:      dwellTime,
+		TripStartTime:  tripStartTime,
+		SpeedThreshold: speedThreshold,
+		DwellDistance:  dwellDistance,
+		Tripping:       false,
+		lastNPoints:    TracksGeoJSON{},
+		intervalPoints: TracksGeoJSON{},
 	}
 }
 
@@ -82,13 +82,20 @@ func (d *TripDetector) AddFeatureToState(f *geojson.Feature) {
 	}
 
 	// Degrade the segment intersection gauge.
-	d.segmentIntersectionGauge *= 0.9
+	n := float64(0)
+	d.intervalPoints.ForEach(func(t *TrackGeoJSON) {
+		if t.MustGetTime().Before(t.MustGetTime().Add(-d.DwellTime)) {
+			return
+		}
+		n += 1.0
+	})
+	d.segmentIntersectionGauge *= 1 - (1.0 / n)
 }
 
 func (d *TripDetector) ResetState() {
 	d.Tripping = false
 	d.MotionStateReason = "reset"
-	d.StoppedLoc = orb.Point{}
+	d.intervalPtsCentroid = orb.Point{}
 	d.lastNPoints = TracksGeoJSON{}
 	d.intervalPoints = TracksGeoJSON{}
 	d.segmentIntersectionGauge = 0
@@ -170,6 +177,8 @@ func segmentsIntersect(segA, segB orb.LineString) bool {
 		i_x := p0_x + (t * s1_x)
 		i_y := p0_y + (t * s1_y)
 		i := orb.Point{i_x, i_y}
+		// IsDetectStopIntersection is considered exclusive to the endpoints of a (either) segment.
+		// My use case is primarily contiguous segments.
 		if !segA.Bound().Min.Equal(i) && !segA.Bound().Max.Equal(i) {
 			return true
 		}
@@ -183,19 +192,6 @@ func segmentsIntersect(segA, segB orb.LineString) bool {
 func (d *TripDetector) AddFeature(f *geojson.Feature) error {
 	t := &TrackGeoJSON{f}
 
-	defer func() {
-		if !d.Tripping {
-			// Update the d.StoppedLoc value to reflect the centroid of d.intervalPoints.
-			pts := []orb.Point{}
-			for _, p := range d.intervalPoints {
-				if p.MustGetTime().Before(t.MustGetTime().Add(-d.DwellTime)) {
-					break
-				}
-				pts = append(pts, p.Point())
-			}
-			d.StoppedLoc, _ = planar.CentroidArea(orb.MultiPoint(pts))
-		}
-	}()
 	defer d.AddFeatureToState(f)
 
 	last := d.LastPointN(0)
@@ -247,110 +243,37 @@ func (d *TripDetector) AddFeature(f *geojson.Feature) error {
 		}
 	}
 
-	/*
-		Experimental: identifying trip ends with track point segment intersections.
-		When knots are introduced to our lines, interpret this as a trip end.
+	weight := detectedNeutral
+	idPC := d.IsDetectStopPointClustering(f)
+	idPCC := d.IsDetectStopPointClusteringCentroid(f)
+	idX := d.IsDetectStopIntersection(f)
+	idRS := d.IsDetectStopReportedSpeeds(f)
 
-	*/
+	d.MotionStateReason = fmt.Sprintf("idPC: %v, idPCC: %v, idX: %v, idRS: %v", idPC, idPCC, idX, idRS)
 
-	/*
-		dwellIntervalPts := d.IntervalPointsWhere(func(tt *TrackGeoJSON) bool {
-			return tt.MustGetTime().After(t.MustGetTime().Add(-d.DwellTime))
-		})
+	weight += idPC
+	weight += idPCC
+	weight += idX
+	weight += idRS
 
-		if len(dwellIntervalPts) > 0 {
-			currentSegment := orb.LineString{
-				dwellIntervalPts[len(dwellIntervalPts)-1].Point(),
-				t.Point(),
-			}
-
-			for i := len(dwellIntervalPts) - 1; i > 0; i-- {
-				segment := orb.LineString{dwellIntervalPts[i-1].Point(), dwellIntervalPts[i].Point()}
-				if segmentsIntersect(segment, currentSegment) {
-					d.segmentIntersectionGauge += 1.0
-					//break
-				}
-			}
-		}
-
-		// If 10% or more of the segments intersect, we are stopped.
-		if d.segmentIntersectionGauge > float64(len(dwellIntervalPts))*0.8 {
-			d.Tripping = false
-			d.MotionStateReason = "segment intersections"
-			return nil
-		}
-	*/
+	// TODO: tinker
+	if weight < detectedStop {
+		d.Tripping = false
+		return nil
+	}
+	if weight > detectedTrip {
+		d.Tripping = true
+		return nil
+	}
 
 	/*
 		d.Tripping = true
 		d.MotionStateReason = "default"
 	*/
 
-	/*
-		Identifying trip ends during normal GPS recording...
-
-		During
-		the normal GPS recording, every point is recorded
-		chronologically. Trip ends usually perform with the
-		point clustering, where sequential GPS points close to
-		each other are in an approximate circle area. To infer
-		this type of trip ends, we adopt k-means clustering algorithm
-		by calculating the maximum distance between
-		any two points in the cluster. We define the diameter of
-		10 m of the circular cluster. If the maximum distance
-		does not exceed this value, the whole cluster will be
-		detected as a potential trip end.
-
-		The first point in the
-		cluster in the order of time is the starting of the trip end
-		and the last point is the terminal of the trip end. In this
-		situation, the dwell time also indicates the minimum
-		duration that a real activity should occur. A proper
-		dwell time should significantly distinguish real trip ends
-		from pseudo ones such as waiting for the traffic signal
-		or greeting the acquaintance during the trip. Based on
-		the specific traffic situations in Shanghai, we assume
-		that a vehicle should be less likely to remain absolutely
-		stationary for a traffic signal or traffic congestion for
-		more than 120 s. Therefore, we take the dwell time of
-		120 s to remove the pseudo trip ends. It is assumed that
-		there does exist a trip end if the duration of the point
-		clustering exceeds 120 s; otherwise, it is treated as the
-		pseudo one and will be removed.
-	*/
-
-	currentTime := t.MustGetTime()
-	dwellStartMin := currentTime.Add(-d.DwellTime)
-
-	maxDist := 0.0
-	dwellExceeded := false
-	pointsMemo := []*geojson.Feature{f}
-outer:
-	for i := len(d.intervalPoints) - 1; i >= 0; i-- {
-		p := d.intervalPoints[i]
-		if p.MustGetTime().Before(dwellStartMin) {
-			dwellExceeded = true
-			break
-		}
-		for _, pp := range pointsMemo {
-			dist := geo.Distance(p.Point(), pp.Point())
-			if dist > maxDist {
-				maxDist = dist
-			}
-			if maxDist > d.StopClusterDistance {
-				// If we ever (within the dwell window) exceed the stop cluster distance, we are done.
-				// The trip is not yet stopped.
-				break outer
-			}
-		}
-	}
-	//speeds := d.intervalPoints.ReportedSpeeds(t.MustGetTime().Add(-d.DwellTime))
-	//reportedMeanSpeeds, _ := stats.Mean(stats.Float64Data(speeds))
-	if dwellExceeded && maxDist <= d.StopClusterDistance /* && reportedMeanSpeeds < d.SpeedThreshold */ {
-		d.Tripping = false
-		d.MotionStateReason = "point clustering"
-		return nil
-	}
+	//if d.IsDetectStopPointClusteringCentroid(f) {
+	//	return nil
+	//}
 
 	/*
 		In addition, some short trip ends may take less than
@@ -411,27 +334,161 @@ outer:
 	// 	}
 	// }
 
-	// Get the average reported speed from all the points in the interval.
-	// Get the average geo-calculated speed from all the points in the interval.
-	// Weight the reported speed 80% and the calculated speed 20%.
-	referenceSpeeds := d.intervalPoints.ReportedSpeeds(t.MustGetTime().Add(-d.TripStartTime))
-	if len(referenceSpeeds) == 0 {
-		return nil
-	}
-	// Get the statistics for this range.
-	referenceStats := stats.Float64Data(referenceSpeeds)
-	mean, err := referenceStats.Mean()
-	if err != nil {
-		return err
-	}
-	if mean > d.SpeedThreshold {
-		d.Tripping = true
-		d.MotionStateReason = "reported speeds"
-		return nil
-	}
-
 	// If we are here, we are UNDECIDED.
 	// The TripDetector maintains its state unchanged.
 
 	return nil
+}
+
+type detectedT float64
+
+const (
+	detectedStop    detectedT = -1
+	detectedNeutral detectedT = 0
+	detectedTrip    detectedT = 1
+)
+
+// IsDetectStopPointClustering is a method that identifies trip ends during normal GPS recording.
+/*
+	Identifying trip ends during normal GPS recording...
+
+	During
+	the normal GPS recording, every point is recorded
+	chronologically. Trip ends usually perform with the
+	point clustering, where sequential GPS points close to
+	each other are in an approximate circle area. To infer
+	this type of trip ends, we adopt k-means clustering algorithm
+	by calculating the maximum distance between
+	any two points in the cluster. We define the diameter of
+	10 m of the circular cluster. If the maximum distance
+	does not exceed this value, the whole cluster will be
+	detected as a potential trip end.
+
+	The first point in the
+	cluster in the order of time is the starting of the trip end
+	and the last point is the terminal of the trip end. In this
+	situation, the dwell time also indicates the minimum
+	duration that a real activity should occur. A proper
+	dwell time should significantly distinguish real trip ends
+	from pseudo ones such as waiting for the traffic signal
+	or greeting the acquaintance during the trip. Based on
+	the specific traffic situations in Shanghai, we assume
+	that a vehicle should be less likely to remain absolutely
+	stationary for a traffic signal or traffic congestion for
+	more than 120 s. Therefore, we take the dwell time of
+	120 s to remove the pseudo trip ends. It is assumed that
+	there does exist a trip end if the duration of the point
+	clustering exceeds 120 s; otherwise, it is treated as the
+	pseudo one and will be removed.
+*/
+func (d *TripDetector) IsDetectStopPointClustering(f *geojson.Feature) (result detectedT) {
+	t := &TrackGeoJSON{f}
+
+	currentTime := t.MustGetTime()
+	dwellStartMin := currentTime.Add(-d.DwellTime)
+
+	maxDist := 0.0
+	dwellExceeded := false
+outer:
+	for i := len(d.intervalPoints) - 1; i >= 0; i-- {
+		p := d.intervalPoints[i]
+		if p.MustGetTime().Before(dwellStartMin) {
+			dwellExceeded = true
+			break
+		}
+		dist := geo.Distance(p.Point(), t.Point())
+		if dist > maxDist {
+			maxDist = dist
+		}
+		if maxDist > d.DwellDistance {
+			// If we ever (within the dwell window) exceed the stop cluster distance, we are done.
+			// The trip is not yet stopped.
+			break outer
+		}
+	}
+	//speeds := d.intervalPoints.ReportedSpeeds(t.MustGetTime().Add(-d.DwellTime))
+	//reportedMeanSpeeds, _ := stats.Mean(stats.Float64Data(speeds))
+	if dwellExceeded && maxDist <= d.DwellDistance /* && reportedMeanSpeeds < d.SpeedThreshold */ {
+		return detectedStop
+	}
+	return detectedTrip
+}
+
+func (d *TripDetector) IsDetectStopPointClusteringCentroid(f *geojson.Feature) (result detectedT) {
+	t := &TrackGeoJSON{f}
+
+	dwellExceeded := false
+
+	// Update the d.intervalPtsCentroid value to reflect the centroid of d.intervalPoints.
+	pts := []orb.Point{t.Point()}
+	// traverse intervalPoints backwards.
+	for i := len(d.intervalPoints) - 1; i >= 0; i-- {
+		p := d.intervalPoints[i]
+		if p.MustGetTime().Before(t.MustGetTime().Add(-d.DwellTime)) {
+			dwellExceeded = true
+			break
+		}
+		pts = append(pts, p.Point())
+	}
+	d.intervalPtsCentroid, _ = planar.CentroidArea(orb.MultiPoint(pts))
+
+	dist := geo.Distance(d.intervalPtsCentroid, t.Point())
+	if dist > d.DwellDistance {
+		return detectedTrip
+	} else if dwellExceeded {
+		return detectedStop
+	}
+	return detectedNeutral
+}
+
+// IsDetectStopIntersection is a method that identifies trip ends with track point segment intersections.
+/*
+	Experimental: identifying trip ends with track point segment intersections.
+	When knots are introduced to our lines, interpret this as a trip end.
+*/
+func (d *TripDetector) IsDetectStopIntersection(f *geojson.Feature) (result detectedT) {
+	t := &TrackGeoJSON{f}
+
+	// Experimental: identifying trip ends with track point segment intersections.
+	// When knots are introduced to our lines, interpret this as a trip end.
+	dwellIntervalPts := d.IntervalPointsWhere(func(tt *TrackGeoJSON) bool {
+		return tt.MustGetTime().After(t.MustGetTime().Add(-d.DwellTime))
+	})
+
+	if len(dwellIntervalPts) > 0 {
+		currentSegment := orb.LineString{
+			dwellIntervalPts[len(dwellIntervalPts)-1].Point(),
+			t.Point(),
+		}
+
+		for i := len(dwellIntervalPts) - 1; i > 0; i-- {
+			segment := orb.LineString{dwellIntervalPts[i-1].Point(), dwellIntervalPts[i].Point()}
+			if segmentsIntersect(segment, currentSegment) {
+				d.segmentIntersectionGauge += 0.25
+				//break
+			}
+		}
+	}
+
+	return detectedT(d.segmentIntersectionGauge * float64(detectedStop))
+}
+
+func (d *TripDetector) IsDetectStopReportedSpeeds(f *geojson.Feature) (result detectedT) {
+	t := &TrackGeoJSON{f}
+
+	referenceSpeeds := d.intervalPoints.ReportedSpeeds(t.MustGetTime().Add(-d.TripStartTime))
+	if len(referenceSpeeds) == 0 {
+		return detectedNeutral
+	}
+	// Get the statistics for this range.
+	referenceStats := stats.Float64Data(referenceSpeeds)
+	mean, _ := referenceStats.Mean()
+	median, _ := referenceStats.Median()
+	if mean > d.SpeedThreshold {
+		return detectedTrip
+	}
+	if median < d.SpeedThreshold {
+		return detectedStop
+	}
+	return detectedNeutral
 }
